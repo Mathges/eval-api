794992ef2ebdeadac7584225595ea1aa4d99f036
couzin <geslinmariecurie@gmail.com>
2023-01-26 23:31:11 +0100

    chore: added validation at task creation

--- app/src/routers/task.js
+++ app/src/routers/task.js
@@ -3,10 +3,12 @@ const controllerHandler = require('../utils/controllerHandler');
 const taskController = require('../controllers/taskController');
 const verifyToken = require('../middlewares/verifyToken');
 const isCompany = require('../middlewares/isCompany');
+const TaskInCreate = require('../utils/validation/schemas/TaskInCreate');
+const bodyValidation = require('../utils/validation/bodyValidation');
 
 const taskRouter = new Router();
 
-taskRouter.post('/create', verifyToken, isCompany, controllerHandler(taskController.create));
+taskRouter.post('/create', verifyToken, isCompany, bodyValidation(TaskInCreate), controllerHandler(taskController.create));
 taskRouter.patch('/add-proposal', verifyToken, isCompany, controllerHandler(taskController.addProposal));
 taskRouter.patch('/handle-proposal-response', verifyToken, controllerHandler(taskController.handleProposalResponse));
 taskRouter.get('', verifyToken, controllerHandler(taskController.getOne));
